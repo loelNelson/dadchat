dd64b3f713f21bf1b80818fa0383031c1806151e
NerfedWizard <loelnelson@gmail.com>
2023-04-26 16:07:41 -0500

    first commit

---
+++ src/main/java/com/loeln/dadchat/DadChatApplication.java
@@ -0,0 +1,13 @@
+package com.loeln.dadchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loeln/dadchat/DadChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loeln.dadchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
